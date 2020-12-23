257f421124ac12985c51140be576247f50f7496d
Aefer <ziyuei2005@gmail.com>
2020-12-24 02:31:21 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-git clone --depth 1 https://github.com/shincrino/mpv-winbuild-cmake.git                                                   
+git clone --depth 1 https://github.com/shincrino/mpv-winbuild-cmake.git
 cd mpv-winbuild-cmake
-mkdir build64                                              cd build64
+mkdir build64
+cd build64
 cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
 ninja gcc
 ninja mpv
