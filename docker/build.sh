2ceb4ea2a9d9e2980071c57a246fa3f2c2a4aeb3
Aefer <ziyuei2005@gmail.com>
2020-12-24 02:15:41 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone --depth 1 https://github.com/skbeh/mpv-winbuild-cmake.git                                                   cd mpv-winbuild-cmake
+git clone --depth=1 https://github.com/skbeh/mpv-winbuild-cmake.git                                                   cd mpv-winbuild-cmake
 mkdir build64                                              cd build64
 cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
 ninja gcc
