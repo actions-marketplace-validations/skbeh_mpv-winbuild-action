9f5ff4f8be9f613d960c12fda26d65918542da57
Aefer <ziyuei2005@gmail.com>
2020-12-24 23:55:30 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ su user
 git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git config --global user.name 'github-actions'
 
-wget https://github.com/shinchiro/mpv-winbuild-cmake/archive/master.zip && unzip master.zip && rm master.zip
+wget https://github.com/skbeh/mpv-winbuild-cmake/archive/master.zip && unzip master.zip && rm master.zip
 
 cd mpv-winbuild-cmake-master
 mkdir build64
