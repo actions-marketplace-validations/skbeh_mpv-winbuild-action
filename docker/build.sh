ec88fab13edfb0496b25c55ff6d9d57931872eaf
Aefer <ziyuei2005@gmail.com>
2020-12-25 18:22:52 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd /home/user
+
 git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git config --global user.name 'github-actions'
 
@@ -10,4 +12,4 @@ cd build64
 cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
 ninja gcc
 ninja mpv
-
+cat ./packages/mpv-prefix/src/mpv-stamp/mpv-configure-*.log
